2f5b29c2888722738e82fab673f857d7b1891b73
Oleg Polyakov <olegpolyakov@outlook.com>
2022-04-09 23:31:02 +0800

    Add BinarySearchTree

---
+++ src/tree/BinarySearchTree.test.js
@@ -0,0 +1,344 @@
+import expect from 'expect';
+
+import BinarySearchTree from './BinarySearchTree.js';
+
+describe('BinarySearchTree', () => {
+    describe('constructor', () => {
+        it('creates an instance of the BinarySearchTree class', () => {
+            const tree = new BinarySearchTree();
+
+            expect(tree).toBeAn(BinarySearchTree);
+        });
+    });
+
+    describe('add', () => {
+        it('adds values', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.size).toBe(4);
+        });
+
+        it('adds values according to the algorithm', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.toString('pre')).toBe('3 1 2 4');
+        });
+    });
+
+    describe('has', () => {
+        it('returns `true` if the value is present', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.has(4)).toBe(true);
+        });
+
+        it('returns `false` if the value is not present', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.has(5)).toBe(false);
+        });
+    });
+
+    describe('remove', () => {
+        it('removes the root node', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.remove(3);
+
+            expect(tree.toString('pre')).toBe('');
+        });
+
+        it('does not remove a node if the value is not present', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.remove(4);
+
+            expect(tree.toString('pre')).toBe('3');
+        });
+
+        it('removes leaf nodes', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            //          3
+            //      1     4
+            //        2
+
+            tree.remove(2);
+            expect(tree.toString('pre')).toBe('3 1 4');
+
+            tree.remove(4);
+            expect(tree.toString('pre')).toBe('3 1');
+
+            tree.remove(1);
+            expect(tree.toString('pre')).toBe('3');
+        });
+
+        it('removes a node with one right child', () => {
+            const tree = new BinarySearchTree();
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            //          3
+            //      1     4
+            //        2
+
+            tree.remove(1);
+
+            expect(tree.toString('pre')).toBe('3 2 4');
+        });
+
+        it('removes a node with one left child', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(2);
+            tree.add(4);
+            tree.add(1);
+
+            //         3
+            //      2     4
+            //    1
+
+            tree.remove(2);
+
+            expect(tree.toString('pre')).toBe('3 1 4');
+        });
+
+        it('removes a node with two children', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(10);
+            tree.add(5);
+            tree.add(4);
+            tree.add(9);
+            tree.add(7);
+            tree.add(8);
+            tree.add(3);
+            tree.add(11);
+
+            //         3
+            //      2     4
+            //    1
+            //          10
+            //      5       11
+            //     4  9
+            //    3  7
+            //        8
+
+            tree.remove(5);
+
+            //          10
+            //      7       11
+            //     4  9
+            //    3  8
+            //        
+
+            expect(tree.toString('pre')).toBe('10 7 4 3 9 8 11');
+        });
+
+        it('removes the root with one left child', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(2);
+            tree.add(1);
+
+            //          3
+            //      2
+            //     1
+
+            tree.remove(3);
+
+            //      2
+            //    1
+
+            expect(tree.toString('pre')).toBe('2 1');
+        });
+
+        it('removes the root with one right child', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(4);
+            tree.add(5);
+
+            //          3
+            //            4
+            //              5
+
+            tree.remove(3);
+
+            //            4
+            //              5
+
+            expect(tree.toString('pre')).toBe('4 5');
+        });
+
+        it('removes the root node with two children', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(10);
+            tree.add(5);
+            tree.add(4);
+            tree.add(6);
+            tree.add(20);
+            tree.add(15);
+            tree.add(12);
+            tree.add(13);
+            tree.add(14);
+
+            //          10
+            //      5        20
+            //    4   6    15
+            //           12
+            //             13
+            //               14
+
+            tree.remove(10);
+
+            //          12
+            //      5        20
+            //    4   6    15
+            //           13
+            //             14
+
+            expect(tree.toString('pre')).toBe('12 5 4 6 20 15 13 14');
+
+            tree.remove(12);
+
+            //          13
+            //      5        20
+            //    4   6    15
+            //           14
+
+            expect(tree.toString('pre')).toBe('13 5 4 6 20 15 14');
+        });
+    });
+
+    describe('forEachPreOrder', () => {
+        it('traverses the tree pre-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            //          3
+            //      1     4
+            //        2
+
+            let string = '';
+
+            tree.forEachPreOrder(value => string += value + ' ');
+
+            expect(string.trim()).toBe('3 1 2 4');
+        });
+    });
+
+    describe('forEachInOrder', () => {
+        it('traverses the tree in-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            //          3
+            //      1     4
+            //        2
+
+            let string = '';
+
+            tree.forEachInOrder(value => string += value + ' ');
+
+            expect(string.trim()).toBe('1 2 3 4');
+        });
+    });
+
+    describe('forEachPostOrder', () => {
+        it('traverses the tree post-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            //          3
+            //      1     4
+            //        2
+
+            let string = '';
+
+            tree.forEachPostOrder(value => string += value + ' ');
+
+            expect(string.trim()).toBe('1 2 4 3');
+        });
+    });
+
+    describe('toString', () => {
+        it('returns a string representation of the tree pre-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.toString('pre')).toBe('3 1 2 4');
+        });
+
+        it('returns a string representation of the tree in-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.toString('in')).toBe('1 2 3 4');
+        });
+
+        it('returns a string representation of the tree post-order', () => {
+            const tree = new BinarySearchTree();
+
+            tree.add(3);
+            tree.add(1);
+            tree.add(4);
+            tree.add(2);
+
+            expect(tree.toString('post')).toBe('1 2 4 3');
+        });
+    });
+});
